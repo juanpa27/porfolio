8a52206b776c727e20477fe6981c2a4c58d73ba2
jbellenzier <jbellenzier@gmail.com>
2024-03-12 14:08:22 -0300

    correcion de analitycs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 import { defineConfig } from 'astro/config';
-import  Analytics  from '@astrojs/analytics'; 
 import tailwind from "@astrojs/tailwind"; 
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  adapter: vercel({
-    webAnalytics: { enabled: true }
-  }),
 
-});  
+});      
