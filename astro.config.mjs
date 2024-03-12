7346e78fe3088dc7016a56b253d093cad86ac572
jbellenzier <jbellenzier@gmail.com>
2024-03-12 13:45:21 -0300

    add analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
+import  Analytics  from '@astrojs/analytics'; 
+import tailwind from "@astrojs/tailwind"; 
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+  integrations: [tailwind()],
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
+
+});  
